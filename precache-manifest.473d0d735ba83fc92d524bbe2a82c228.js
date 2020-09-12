16e15817c952785dc178a635a040fcf052560cdf
ganesh-papola <developer.ganeshpapola@gmail.com>
2020-09-12 20:22:56 +0530

    Updates

---
+++ precache-manifest.473d0d735ba83fc92d524bbe2a82c228.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3ee315f568414164350ef6204be834",
+    "url": "/wedding-blizz/index.html"
+  },
+  {
+    "revision": "cac405d3c39171fcfabb",
+    "url": "/wedding-blizz/static/css/main.6558917a.chunk.css"
+  },
+  {
+    "revision": "9c5c6db9508d2dda80bc",
+    "url": "/wedding-blizz/static/js/2.dcca34a0.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/wedding-blizz/static/js/2.dcca34a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac405d3c39171fcfabb",
+    "url": "/wedding-blizz/static/js/main.6bc12a3d.chunk.js"
+  },
+  {
+    "revision": "92c0e4a823488855bbcb",
+    "url": "/wedding-blizz/static/js/runtime-main.381313c9.js"
+  },
+  {
+    "revision": "c81f697f4bb9340bbc60fd1185d67509",
+    "url": "/wedding-blizz/static/media/CormorantInfant-Bold.c81f697f.ttf"
+  },
+  {
+    "revision": "0286a9a2112f231ba5858a50b9e994c4",
+    "url": "/wedding-blizz/static/media/CormorantInfant-BoldItalic.0286a9a2.ttf"
+  },
+  {
+    "revision": "32cf6bb4aaa744271b198fb6055b3e39",
+    "url": "/wedding-blizz/static/media/FamilyFriends.32cf6bb4.png"
+  },
+  {
+    "revision": "022af2f62a7c0af68de51fa2cbf218e2",
+    "url": "/wedding-blizz/static/media/Gotham-Medium.022af2f6.ttf"
+  },
+  {
+    "revision": "f76e3adf545b3299f643fd7642800351",
+    "url": "/wedding-blizz/static/media/GothamLight.f76e3adf.otf"
+  },
+  {
+    "revision": "bc25c7fbad0c1a7dcc4a08036e4e5250",
+    "url": "/wedding-blizz/static/media/PlanningBasics.bc25c7fb.png"
+  },
+  {
+    "revision": "5519ca32c9fc422339c487d935bd1125",
+    "url": "/wedding-blizz/static/media/WeddingFashion.5519ca32.png"
+  },
+  {
+    "revision": "f660a017315fd5990e9c120268301902",
+    "url": "/wedding-blizz/static/media/WeddingReception.f660a017.png"
+  },
+  {
+    "revision": "39541bb16d429b54e3b541579fa013f4",
+    "url": "/wedding-blizz/static/media/WeddingService.39541bb1.png"
+  },
+  {
+    "revision": "ad52a46e4f3df2bbf3ff2a3ab15da433",
+    "url": "/wedding-blizz/static/media/WeddingTheme.ad52a46e.png"
+  },
+  {
+    "revision": "f667593283bbbb808f01266c9bfd82c7",
+    "url": "/wedding-blizz/static/media/calculator.f6675932.svg"
+  },
+  {
+    "revision": "f8b9905bbc220bb70270f9b5779baff4",
+    "url": "/wedding-blizz/static/media/clipboard.f8b9905b.svg"
+  },
+  {
+    "revision": "c61bfc049cf5aee5cc1c80ec890c65e0",
+    "url": "/wedding-blizz/static/media/downloadAndroid.c61bfc04.svg"
+  },
+  {
+    "revision": "f74b96bd3299d569f2b766e8d8f9550c",
+    "url": "/wedding-blizz/static/media/downloadApple.f74b96bd.svg"
+  },
+  {
+    "revision": "2ac5cbf84d8b79b2842da2eb770107ab",
+    "url": "/wedding-blizz/static/media/downloadPrAndroid.2ac5cbf8.svg"
+  },
+  {
+    "revision": "e115e51cc3ab6aaa847bbc977cd27af9",
+    "url": "/wedding-blizz/static/media/downloadPrApple.e115e51c.svg"
+  },
+  {
+    "revision": "5fedef2438ebae3bba002b22c12a37dc",
+    "url": "/wedding-blizz/static/media/grid.5fedef24.svg"
+  },
+  {
+    "revision": "2ce089cd53ddc9076b1a2f687482e638",
+    "url": "/wedding-blizz/static/media/heart.2ce089cd.svg"
+  },
+  {
+    "revision": "3b9e218f1b42a585abea5dcffeae4162",
+    "url": "/wedding-blizz/static/media/logo-text.3b9e218f.svg"
+  },
+  {
+    "revision": "fb75297dc40d8c7e01d22a2ab078bbdf",
+    "url": "/wedding-blizz/static/media/logoF.fb75297d.svg"
+  },
+  {
+    "revision": "43d903fe213e02a7a9f3fdd7dc15ff62",
+    "url": "/wedding-blizz/static/media/perfectWedding1.43d903fe.png"
+  },
+  {
+    "revision": "32e0e351e1068381c175ea0a5e860d5a",
+    "url": "/wedding-blizz/static/media/perfectWedding2.32e0e351.png"
+  },
+  {
+    "revision": "aebf32873cc4d03cf795458781d9b5c2",
+    "url": "/wedding-blizz/static/media/userTie.aebf3287.svg"
+  },
+  {
+    "revision": "f31f442abc27674e9e4cb5b50780cbd0",
+    "url": "/wedding-blizz/static/media/users.f31f442a.svg"
+  },
+  {
+    "revision": "036ec4c4c65fcec64b09be383a21dbda",
+    "url": "/wedding-blizz/static/media/wedding-rings.036ec4c4.svg"
+  },
+  {
+    "revision": "12df74236d34bf2666a61cea554fecf9",
+    "url": "/wedding-blizz/static/media/weddingPlannerFront1.12df7423.png"
+  },
+  {
+    "revision": "57fbbca83e8af5dc987037df9f906aa0",
+    "url": "/wedding-blizz/static/media/weddingPlannerFront2.57fbbca8.png"
+  },
+  {
+    "revision": "ffdb66340a197c6d049eae638a15044f",
+    "url": "/wedding-blizz/static/media/weddingVendor.ffdb6634.png"
+  }
+]);

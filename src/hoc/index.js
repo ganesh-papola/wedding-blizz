b6ffad9e69510ede476c2050e52764b4d148b4f0
ganesh-papola <developer.ganeshpapola@gmail.com>
2020-09-26 03:28:27 +0530

    added firebase login/signup, css fixes

--- src/hoc/index.js
+++ src/hoc/index.js
@@ -4,9 +4,11 @@ import { Header, Footer } from 'containers';
 export default props => {
   return (
     <Fragment>
-        <Header />
+      <Header />
+      <div style={{ minHeight: window.innerHeight / 1.35 }}>
         {props.children}
-        <Footer />
+      </div>
+      <Footer />
     </Fragment>
   )
 }
